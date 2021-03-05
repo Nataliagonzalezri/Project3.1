f3bb8ff69816c7eca8ea8ddf788d649fa249d6c0
Natalia Gonzalez <nataliagonzalezri@gmail.com>
2021-03-05 15:41:24 -0300

    Use of for

---
+++ Project 3.7 (uses of for).py
@@ -0,0 +1,11 @@
+# in the range, left included, right not included.
+for i in range(0, 20):
+    print("In the iteration with the value", i)
+for i in range(0, 20):
+    print("perfect square of", i, "is", i*i)
+for first_factor in range(1, 11):
+    for second_factor in range (1, 11):
+        print(first_factor, "x", second_factor, "=", first_factor*second_factor)
+
+
+
