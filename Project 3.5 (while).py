601dbb9acfa6278fd4f50f514b603d3fc1395710
Natalia Gonzalez <nataliagonzalezri@gmail.com>
2021-03-07 08:47:50 -0300

    while

---
+++ Project 3.5 (while).py
@@ -0,0 +1,8 @@
+# the sum of the first 9 numbers
+n = 1
+result = 0
+while n < 10:
+    if n % 2 == 1:
+        result = result + n
+    n = n + 1
+print("sum of the first 9 numbers that are odd is", result)
